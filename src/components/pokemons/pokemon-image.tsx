2945cf7fec9b8bcb2cb5a43241be5d0b5b13da31
Ivan Torrez <itorrez1506@gmail.com>
2023-05-23 03:50:43 -0400

    first commit

---
+++ src/components/pokemons/pokemon-image.tsx
@@ -0,0 +1,42 @@
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
+
+interface Props {
+  id: number;
+  size?: number;
+  backImage: boolean;
+  isVisible?: boolean;
+}
+
+export const PokemonImage = component$(
+  ({ id, size = 200, backImage = false, isVisible }: Props) => {
+    const imageLoaded = useSignal(false);
+
+    useTask$(({ track }) => {
+      track(() => id);
+      imageLoaded.value = false;
+    });
+
+    
+    return (
+      <div
+        class="flex items-center justify-center"
+        style={{ width: `${size}px`, height: `${size}px` }}
+      >
+        {!imageLoaded.value && (<span>Cargando...</span>)}
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+            backImage ? "back/" : ""
+          }${id}.png`}
+          alt="Pokemon sprite"
+          width={`${size}px`}
+          height={`${size}px`}
+          onLoad$={() => (imageLoaded.value = true)}
+          class={[
+            { "hidden": !imageLoaded.value, "brightness-0": isVisible },
+            "transition-all",
+          ]}
+        />
+      </div>
+    );
+  }
+);
